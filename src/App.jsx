71c72eabe72d15fec5ed3a61f4a0618fc62d327b
javier.reyes@gruascarter.com <javier.reyes@gruascarter.com>
2025-12-17 07:18:05 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header.jsx';
 import MovieList from './components/MoviesList/MovieList.jsx';
